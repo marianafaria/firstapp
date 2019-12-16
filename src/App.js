3e302eb8d1a83e6d72bc3a73f8569b391c847cf6
Mariana <mariana.araujo@credicom.com.br>
2019-12-16 16:19:33 -0300

    Carousel e CardProducts

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import NavBar from './components/NavBar'
+import Carousel from './components/CarouselHome'
+import CardsProducts from './components/CardsProducts'
+import {Container, Row} from 'react-bootstrap'
 
 function App() {
   return (
     <>
     <NavBar/>
+    <Carousel/>
+    <Container>
+      <Row>
+        <CardsProducts/>
+      </Row>
+    </Container>
     </>
   );
 }
